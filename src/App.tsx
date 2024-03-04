c8106aba4d3d18256b93d4b52bf6960a43eebf04
valentindj <valentindj@starway-media.com>
2024-03-04 13:54:19 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const App = () => {
+    return (
+        <main className={"container text-center mx-auto my-0"}>
+            <h1 className={"underline text-4xl"}>Hello react-ts-tailwind!</h1>
+        </main>
+    );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+import "./style/index.css";
+
+// Clear the existing HTML content
+document.body.innerHTML = "<div id=\"app\"></div>";
+
+// Render your React component instead
+const root = createRoot(document.getElementById("app")!);
+root.render(<App/>);
